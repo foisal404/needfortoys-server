1c07a4c6e9791ebbeb9f09b5de5bd336fb76da4c
foisal404 <foisalahmmed2@gmail.com>
2023-05-21 05:08:43 +0600

    single  toy  data using get method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app=express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port=process.env.PORT || 5000;
 
@@ -36,6 +36,15 @@ async function run() {
     })
     app.get('/toys',async(req,res)=>{
       const result =await toysCollection.find().toArray();
+      res.send(result)
+    })
+
+    // specific one toy using id
+    app.get('/toys/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      // console.log(result)
       res.send(result)
     })
     // Send a ping to confirm a successful connection
