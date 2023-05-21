0d12d7d1e1b6c9b5a130572d789375cf7017b1b6
foisal404 <foisalahmmed2@gmail.com>
2023-05-21 22:10:13 +0600

    sepecic toy delate usind delete method

--- index.js
+++ index.js
@@ -60,6 +60,15 @@ async function run() {
       // console.log(result)
       res.send(result)
     })
+
+    // specific one toy delate  using id
+    app.delete('/toys/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.deleteOne(query);
+      // console.log(result)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
