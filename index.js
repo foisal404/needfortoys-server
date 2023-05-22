0eebc14d1a77e94d0679b4e20b1da61926c42281
foisal404 <foisalahmmed2@gmail.com>
2023-05-22 11:32:55 +0600

    vercel problem solved

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const port=process.env.PORT || 5000;
 
 //middleware 
 app.use(cors())
+// const corsConfig = {
+//   origin: '*',
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE']
+//   }
+//   app.use(cors(corsConfig))
+//   app.options("", cors(corsConfig))
 app.use(express.json())
 
 
@@ -25,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("toysDB");
     const categoriesCollection = database.collection("categories");
